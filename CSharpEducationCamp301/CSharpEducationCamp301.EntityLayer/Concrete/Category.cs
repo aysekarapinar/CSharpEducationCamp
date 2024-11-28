db75e95e8dab8d0fd928a3b8ba0d967c5340fc98
Ayşe Karapinar <164161988+aysekarapinar@users.noreply.github.com>
2024-11-28 18:46:57 +0300

    Add files via upload

---
+++ CSharpEducationCamp301/CSharpEducationCamp301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEducationCamp301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
+/*
+    Field-Variable-Property
+*/
+
+/*
+ * int x; --> Direkt sınıfın içine tanımlanırsa Field olur
+*/
+/*
+ get, set alırsa Property olur.
+ */
+/*
+ Bir değer method içinde tanımlanırsa Variable olur.
+*/
+
